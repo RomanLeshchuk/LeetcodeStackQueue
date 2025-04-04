3aec178c6544a0fc76d695f8cf2d9c798d42681d
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-04-04 23:22:03 +0300

    Small refactoring: made attributes protected

--- MaximumFrequencyStack/FreqStack.py
+++ MaximumFrequencyStack/FreqStack.py
@@ -2,26 +2,26 @@ from collections import defaultdict
 
 class FreqStack:
     def __init__(self):
-        self.stacks_by_frequency = defaultdict(list)
-        self.frequency_of_vals = dict()
-        self.max_frequency = 0
+        self._stacks_by_frequency = defaultdict(list)
+        self._frequency_of_vals = dict()
+        self._max_frequency = 0
 
     def push(self, val: int) -> None:
-        if val not in self.frequency_of_vals:
-            self.frequency_of_vals[val] = 1
+        if val not in self._frequency_of_vals:
+            self._frequency_of_vals[val] = 1
         else:
-            self.frequency_of_vals[val] += 1
+            self._frequency_of_vals[val] += 1
 
-        self.max_frequency = max(self.max_frequency, self.frequency_of_vals[val])
-        self.stacks_by_frequency[self.frequency_of_vals[val]].append(val)
+        self._max_frequency = max(self._max_frequency, self._frequency_of_vals[val])
+        self._stacks_by_frequency[self._frequency_of_vals[val]].append(val)
 
     def pop(self) -> int:
-        res = self.stacks_by_frequency[self.max_frequency][-1]
-        self.stacks_by_frequency[self.max_frequency].pop()
+        res = self._stacks_by_frequency[self._max_frequency][-1]
+        self._stacks_by_frequency[self._max_frequency].pop()
 
-        self.frequency_of_vals[res] -= 1
+        self._frequency_of_vals[res] -= 1
 
-        while self.max_frequency and not self.stacks_by_frequency[self.max_frequency]:
-            self.max_frequency -= 1
+        while self._max_frequency and not self._stacks_by_frequency[self._max_frequency]:
+            self._max_frequency -= 1
 
         return res
